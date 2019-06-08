43dfa022e9bfdf616ec8da12d38ccffe1dc14b5f
zanonera <ricardo_zanone@yahoo.com.br>
2019-06-08 12:33:57 -0300

    challenge 6 done, 7 in progress

--- 6.py
+++ 6.py
@@ -1,3 +1,25 @@
 # Challenge 6
 #http://www.pythonchallenge.com/pc/def/channel.html
 #http://www.pythonchallenge.com/pc/def/channel.zip
+import re
+
+# importing required modules 
+from zipfile import ZipFile 
+  
+# specifying the zip file name 
+file_name = "channel.zip"
+archive = ZipFile(file_name, 'r')
+
+txt="90052.txt"
+getComments=''
+# opening the zip file in READ mode 
+with ZipFile(file_name, 'r') as zip: 
+   for i in range(0,1000):
+      with zip.open(txt) as my_file:
+         getComments+=archive.getinfo(txt).comment.decode("utf-8")   #decode removes the b'' from string
+         s=str(my_file.read())
+         print(s)
+         s=str(re.findall("(?i)next nothing is (\d+)",s))   #ignore the case in the string, get all the decimals
+         txt=str(s.strip("['']"))+'.txt'
+         print(txt)
+         print(getComments)
