70f90d8e04afc810808e6bace15c2c51d8f005a9
zanonera <ricardo_zanone@yahoo.com.br>
2019-06-11 23:28:51 -0300

    Update on Challenge 10

--- 10.py
+++ 10.py
@@ -1,3 +1,10 @@
 #Challenge 10
 #http://www.pythonchallenge.com/pc/return/cow.html
 #http://www.pythonchallenge.com/pc/return/bull.html
+
+# sequence.txt:
+#   a = [1, 11, 21, 1211, 111221,
+
+# The sequence is a "look-and-say" sequence:
+#https://en.wikipedia.org/wiki/Look-and-say_sequence  
+
